66763aa2bf4dec2925f0f8dfba05d70a756581e8
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-01-05 19:11:39 +0200

    all tasks

---
+++ js/task-13.js
@@ -0,0 +1,24 @@
+class StringBuilder {
+  #value;
+
+  constructor(str) {
+    this.#value = str;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
+  }
+}
